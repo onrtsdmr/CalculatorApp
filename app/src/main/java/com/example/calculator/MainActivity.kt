de190061277e3d39c31b12bec0d248a714e917fd
OnurTAŞDEMİR <onurtaademir@gmail.com>
2019-07-07 21:26:51 +0300

    Fixed some bugs.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,262 +3,47 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    var number1: Double? = null
-
-    var number2: Double? = null
-
-    var operation: String? = null
-
-    var isMaths: Boolean = true
-
+    private var number1: Double? = null
+    private var number2: Double? = null
+    private var operation: String? = null
     override fun onClick(v: View?) {
-
-
         when (v!!.id) {
-
-
-            R.id.img_one -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}1"
-                txt_hsp.setText(newText)
-
-            }
-
-            R.id.img_two -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}2"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_three -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}3"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_four -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}4"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_five -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}5"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_six -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}6"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_seven -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}7"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_eight -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}8"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_nine -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}9"
-                txt_hsp.setText(newText)
-            }
-
-            R.id.img_zero -> {
-
-                val getText = txt_hsp.text
-                val newText = "${getText}0"
-                txt_hsp.setText(newText)
-
-            }
-
+            R.id.img_one -> setText("1")
+            R.id.img_two -> setText("2")
+            R.id.img_three -> setText("3")
+            R.id.img_four -> setText("4")
+            R.id.img_five -> setText("5")
+            R.id.img_six -> setText("6")
+            R.id.img_seven -> setText("7")
+            R.id.img_eight -> setText("8")
+            R.id.img_nine -> setText("9")
+            R.id.img_zero -> setText("0")
             R.id.img_c -> clear()
-
-            R.id.img_percent -> {
-
-                val txt: String = (txt_hsp.text.toString().toDouble() / 100).toString()
-                txt_hsp.setText(txt)
-
-            }
-
-            R.id.img_division -> {
-                number1 = txt_hsp.text.toString().toDouble()
-                operation = "/"
-                clear()
-                nmbr1.setText(number1.toString())
-                nmbr2.setText("")
-                oprt.setText("/")
-                img_plus.isClickable = false
-                img_minus.isClickable = false
-                img_division.isClickable = false
-                img_multiply.isClickable = false
-
-            }
-
-            R.id.img_multiply -> {
-                number1 = txt_hsp.text.toString().toDouble()
-                operation = "*"
-                clear()
-                nmbr1.setText(number1.toString())
-                nmbr2.setText("")
-                oprt.setText("*")
-                img_plus.isClickable = false
-                img_minus.isClickable = false
-                img_division.isClickable = false
-                img_multiply.isClickable = false
-            }
-
-            R.id.img_minus -> {
-                number1 = txt_hsp.text.toString().toDouble()
-                operation = "-"
-                clear()
-                nmbr1.setText(number1.toString())
-                nmbr2.setText("")
-                oprt.setText("-")
-                img_plus.isClickable = false
-                img_minus.isClickable = false
-                img_division.isClickable = false
-                img_multiply.isClickable = false
-            }
-
-            R.id.img_plus -> {
-                number1 = txt_hsp.text.toString().toDouble()
-                operation = "+"
-                clear()
-                nmbr1.setText(number1.toString())
-                nmbr2.setText("")
-                oprt.setText("+")
-                img_plus.isClickable = false
-                img_minus.isClickable = false
-                img_division.isClickable = false
-                img_multiply.isClickable = false
-            }
-
-            R.id.img_equal -> {
-
-                if (number1 != null) {
-                    when (operation) {
-
-                        "+" -> {
-                            number2 = txt_hsp.text.toString().toDouble()
-                            clear()
-                            nmbr2.setText(number2.toString())
-                            txt_hsp.setText((number1!! + number2!!).toString())
-                            img_plus.isClickable = true
-                            img_minus.isClickable = true
-                            img_division.isClickable = true
-                            img_multiply.isClickable = true
-                            isMaths = true
-                        }
-                        "-" -> {
-                            number2 = txt_hsp.text.toString().toDouble()
-                            clear()
-                            nmbr2.setText(number2.toString())
-                            txt_hsp.setText((number1!! - number2!!).toString())
-                            img_plus.isClickable = true
-                            img_minus.isClickable = true
-                            img_division.isClickable = true
-                            img_multiply.isClickable = true
-                            isMaths = true
-                        }
-                        "/" -> {
-                            number2 = txt_hsp.text.toString().toDouble()
-                            clear()
-                            nmbr2.setText(number2.toString())
-                            txt_hsp.setText((number1!! / number2!!).toString())
-                            img_plus.isClickable = true
-                            img_minus.isClickable = true
-                            img_division.isClickable = true
-                            img_multiply.isClickable = true
-                            isMaths = true
-                        }
-                        "*" -> {
-                            number2 = txt_hsp.text.toString().toDouble()
-                            clear()
-                            nmbr2.setText(number2.toString())
-                            txt_hsp.setText((number1!! * number2!!).toString())
-                            img_plus.isClickable = true
-                            img_minus.isClickable = true
-                            img_division.isClickable = true
-                            img_multiply.isClickable = true
-                            isMaths = true
-                        }
-
-
-
-
-
-                    }
-                }
-
-
-            }
-
-            R.id.img_maths -> {
-                if (txt_hsp.text.isEmpty()!=true && txt_hsp.text!=0.toString()) {
-                    if (isMaths) {
-
-                        val txt = "-${txt_hsp.text}"
-                        txt_hsp.setText(txt)
-                        isMaths = false
-
-
-                    } else {
-
-                        val txt = ((txt_hsp.text.toString().toDouble()) * -1).toString()
-                        txt_hsp.setText(txt)
-                        isMaths = true
-                    }
-                }
-            }
-            R.id.txt_comma -> {
-
-                val txt = "${txt_hsp.text}."
-                txt_hsp.setText(txt)
-                txt_comma.isClickable = false
-
-            }
-
-            R.id.img_squareroot ->{
-                val txt = Math.sqrt(txt_hsp.text.toString().toDouble()).toString()
-                txt_hsp.setText(txt)
-            }
-
+            R.id.img_percent -> percent()
+            R.id.img_division -> operationsFalse(oprt, "/")
+            R.id.img_multiply -> operationsFalse(oprt, "*")
+            R.id.img_minus -> operationsFalse(oprt, "-")
+            R.id.img_plus -> operationsFalse(oprt, "+")
+            R.id.img_equal -> equal()
+            R.id.img_maths -> maths()
+            R.id.txt_comma -> comma()
+            R.id.img_squareroot -> squareroot()
         }
-
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txt_hsp.text = 0.toString()
+        hesapla()
+    }
 
-
-
-
+    private fun hesapla() {
         img_one.setOnClickListener(this)
         img_two.setOnClickListener(this)
         img_three.setOnClickListener(this)
@@ -281,10 +66,96 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         txt_comma.setOnClickListener(this)
     }
 
-
-    fun clear() {
-        txt_hsp.setText(0.toString())
+    private fun clear() {
+        txt_hsp.text = 0.toString()
     }
 
-}
+    private fun equal() {
+        if (number1 != null) {
+            when (operation) {
+                "+" -> {
+                    number2 = txt_hsp.text.toString().toDouble()
+                    clear()
+                    nmbr2.text = number2.toString()
+                    txt_hsp.text = (number1!! + number2!!).toString()
+                    operationsTrue()
+                }
+                "-" -> {
+                    number2 = txt_hsp.text.toString().toDouble()
+                    clear()
+                    nmbr2.text = number2.toString()
+                    txt_hsp.text = (number1!! - number2!!).toString()
+                    operationsTrue()
+                }
+                "/" -> {
+                    number2 = txt_hsp.text.toString().toDouble()
+                    clear()
+                    nmbr2.text = number2.toString()
+                    txt_hsp.text = (number1!! / number2!!).toString()
+                    operationsTrue()
+                }
+                "*" -> {
+                    number2 = txt_hsp.text.toString().toDouble()
+                    clear()
+                    nmbr2.text = number2.toString()
+                    txt_hsp.text = (number1!! * number2!!).toString()
+                    operationsTrue()
+                }
+            }
+        }
+    }
 
+    private fun operationsTrue() {
+        img_plus.isClickable = true
+        img_minus.isClickable = true
+        img_division.isClickable = true
+        img_multiply.isClickable = true
+    }
+
+    private fun operationsFalse(oprt: TextView, oprtn: String) {
+        number1 = txt_hsp.text.toString().toDouble()
+        operation = oprtn
+        clear()
+        nmbr1.text = number1.toString()
+        nmbr2.text = ""
+        oprt.text = oprtn
+        img_plus.isClickable = false
+        img_minus.isClickable = false
+        img_division.isClickable = false
+        img_multiply.isClickable = false
+    }
+
+    private fun maths() {
+        if (txt_hsp.text.isNotEmpty() && txt_hsp.text != 0.toString()) {
+            if (txt_hsp.text.toString().toDouble() > 0) {
+                val txt = "-${txt_hsp.text}"
+                txt_hsp.text = txt
+            } else {
+                val txt = ((txt_hsp.text.toString().toDouble()) * -1).toString()
+                txt_hsp.text = txt
+            }
+        }
+    }
+
+    private fun comma() {
+        val txt = "${txt_hsp.text}."
+        txt_hsp.text = txt
+        txt_comma.isClickable = false
+    }
+
+    private fun squareroot() {
+        val txt = sqrt(txt_hsp.text.toString().toDouble()).toString()
+        txt_hsp.text = txt
+    }
+
+    private fun setText(nmbr: String) {
+        val getText = txt_hsp.text
+        val setText = "$getText$nmbr"
+        txt_hsp.text = setText
+    }
+
+    private fun percent() {
+        val txt: String = (txt_hsp.text.toString().toDouble() / 100).toString()
+        txt_hsp.text = txt
+    }
+}
